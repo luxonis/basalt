a32b736315cf2ad31005a5e058f6d1ad25d2afe9
Serafadam <serafadam@gmail.com>
2024-06-12 09:34:56 +0200

    remove debug prints

--- include/basalt/optical_flow/optical_flow.h
+++ include/basalt/optical_flow/optical_flow.h
@@ -148,17 +148,6 @@ class OpticalFlowBase {
   virtual void processingLoop() = 0;
 
   void start() { 
-    if(processing_thread==nullptr){
-      std::cout << "processing_thread is null" << std::endl;
-    } else {
-      std::cout << "processing_thread is not null" << std::endl;
-    }
-    processing_thread.reset();
-      if(processing_thread==nullptr){
-      std::cout << "processing_thread is null" << std::endl;
-    } else {
-      std::cout << "processing_thread is not null" << std::endl;
-    }
     processing_thread =  std::make_unique<std::thread>(&OpticalFlowBase::processingLoop, this);
     }
 
