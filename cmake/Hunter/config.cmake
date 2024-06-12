ec75517a8a27dd77baf2b3bfea1f4e23f558b134
Serafadam <serafadam@gmail.com>
2024-06-12 11:34:51 +0200

    remove rest of shared libs

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -28,8 +28,6 @@ hunter_config(
     VERSION 1.22.10
     URL "https://github.com/luxonis/Sophus/archive/refs/heads/hunterized.zip"
     SHA1 "ff17d3dce1bcadabbc9fb83845a29974377d7f95" 
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=ON
         BUILD_SOPHUS_TESTS=OFF
         BUILD_SOPHUS_EXAMPLES=OFF
 )
@@ -39,15 +37,11 @@ hunter_config(
     VERSION 1.3.0
     URL "https://github.com/USCiLab/cereal/archive/64f50dbd5cecdaba785217e2b0aeea3a4f1cdfab.zip"
     SHA1 "6587337e36045f1b5f68c902f5e04a76d8999e02"
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=ON
 )
 
 hunter_config(
     fmt
     VERSION 8.1.1
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=ON
     )
 
 
@@ -56,8 +50,6 @@ hunter_config(
     VERSION 0.7.3
     URL "https://github.com/Neargye/magic_enum/archive/3d1f6a5a2a3fbcba077e00ad0ccc2dd9fefc2ca7.zip"
     SHA1 "c9a27f6ff8311f0c6b2adb959d0598f079fcc9f3"
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=ON
 )
 
 hunter_config(
@@ -65,6 +57,4 @@ hunter_config(
     VERSION 1.0.0
     URL "https://github.com/luxonis/opengv/archive/refs/heads/master.zip"
     SHA1 "5898069592f3e7484d9e83d918dad9287876d103"
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=ON
 )
